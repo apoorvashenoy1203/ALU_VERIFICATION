9656391b388b3d8e87c286ef91b753ab88293f8d
Apoorva shenoy <136146978+apoorvashenoy1203@users.noreply.github.com>
2025-07-30 22:48:50 +0530

    Create alu_pkg.sv

---
+++ src/alu_pkg.sv
@@ -0,0 +1,10 @@
+    package alu_pkg;
+  `include "alu_transaction.sv"
+  `include "alu_generator.sv"
+  `include "alu_driver.sv"
+  `include "alu_monitor.sv"
+  `include "ref_model.sv"
+  `include "scoreboard.sv"
+  `include "alu_environment.sv"
+  `include "alu_test.sv"
+endpackage
